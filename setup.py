b3e029be55986f7a72ed5e457d2a6d3b07eabcd2
EJ Silva <silvajr.efren@g.batastate-u.edu.ph>
2020-09-15 22:49:22 +0800

    edit setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name='Internet Connection Recorder',
     version='1.0',
     py_modules=['yourscript'],
-    install_requires=[
-        'Click',
-    ],
     entry_points='''
         [console_scripts]
         record=main:set_params
