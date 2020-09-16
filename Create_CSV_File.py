e65f0398d2eaa6df711aff005238b95f2128e2cb
benaiahnavarro <benaiahnavarro@gmail.com>
2020-09-16 16:44:31 +0800

    Create_CSV_File

---
+++ Create_CSV_File.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+
+
+def create_csv(data, file_name):
+    """Collect the results, tabulate it and will create a csv file
+
+    Parameters:
+    The parameter in the table are Ping, Upload and Download
+
+    Returns:
+    It will return the value from the arguments provided and create a csv file of the results in a tabular form
+
+   """
+    speedtest = {'Ping': [data['ping']],
+                 'Upload': [data['upload']],
+                 'Download': [data['download']]}
+
+    table = pd.DataFrame(speedtest, columns=['Ping', 'Upload', 'Download'])
+
+    name = '/' + file_name + '.csv'
+
+    if not os.path.exists(r'C:\Users\benai\Documents\internet-connection-forecaster' + name):
+        table.to_csv(r'C:\Users\benai\Documents\internet-connection-forecaster' +
+                     name, index=False, header=True)
+    else:
+        table.to_csv(r'C:\Users\benai\Documents\internet-connection-forecaster' +
+                     name, index=False, mode='a', header=False)
