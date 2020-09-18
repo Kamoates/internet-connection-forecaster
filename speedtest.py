36bd5a582562d89997b0181aa22a62d931be3a4a
EJ Silva <silvajr.efren@g.batastate-u.edu.ph>
2020-09-18 17:20:06 +0800

    fixed speedtest-cli

--- speedtest.py
+++ speedtest.py
@@ -1,5 +1,7 @@
-import pyspeedtest as st
 import time
+import subprocess
+from collections import defaultdict
+
 
 def check_speed(duration, intervals):
     """Summary or Description of the Function
@@ -11,8 +13,30 @@ def check_speed(duration, intervals):
         int:Returning value
    """
     number_of_loops = duration//intervals
+    speedtest_command = 'speedtest-cli --csv'
+
+    # getting headers
+    headers = subprocess.Popen('speedtest-cli --csv-header',
+                               stdout=subprocess.PIPE).stdout.readline().rstrip().decode('UTF-8').split(',')
+
+    # initialized dictionary
+    result_dictionary = defaultdict(list)
+
     for _ in range(number_of_loops):
-        
+        print('Running ...')
+        raw_result = subprocess.Popen(
+            speedtest_command, stdout=subprocess.PIPE).stdout
+        result = raw_result.readline().rstrip().decode('UTF-8').split(',')
+
+        # add result to the dictionary
+        for header, value in zip(headers, result):
+            result_dictionary[header].append(value)
+
+        print('Done!\n')
+
         time.sleep(intervals)
 
-check_speed(5, 2)
+    print(result_dictionary)
+
+
+check_speed(10, 2)
